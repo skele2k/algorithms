77baef1a9b97cfe9525418221278578354454d60
skele2k <tsatsralt2000@gmail.com>
2022-05-06 18:53:22 +0900

    added command line input

--- euclidian.cpp
+++ euclidian.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits.h>
 
 using namespace std;
 
@@ -11,6 +12,23 @@ int gcd(int a, int b) {
     return gcd(b, a%b);
 }
 
-int main() {
-    cout << gcd(76,16) << endl;
+int main(int argc, char ** argv) {
+    if (argc != 3) {
+        cout << "Please insert two input" << endl;
+        return -1;
+    } 
+    // cout << argv[1] << " " << argv[2] << endl;
+    char *p;
+    long firstParameter = strtol(argv[1], &p, 10);
+    long secondParameter = strtol(argv[2], &p, 10);
+
+    // Check for errors: e.g., the string does not represent an integer
+    // or the integer is larger than int
+    if (errno != 0 || *p != '\0' || firstParameter > INT_MAX || firstParameter < INT_MIN) {
+        // Put here the handling of the error, like exiting the program with
+        // an error message
+    } else {
+        // No error
+        cout << "GCD: "<< gcd(firstParameter, secondParameter) << endl;
+    }
 }
